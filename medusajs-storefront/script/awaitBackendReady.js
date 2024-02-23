f8470a7b68215089c7cb5ee310b6e70f8094f1cd
Rasmus Puls <rasmus@triggerz.net>
2024-02-23 22:00:21 +0100

    expect status 200, since 404 gives false positives

--- medusajs-storefront/script/awaitBackendReady.js
+++ medusajs-storefront/script/awaitBackendReady.js
@@ -1,11 +1,11 @@
 const nodefetch = require('node-fetch');
 
 const checkBackend = () => {
-  const url = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || 'http://localhost:9000';
+  const url = (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL || 'http://localhost:9000') + '/store/product-categories?expand=category_children';
   nodefetch(url)
     .then((res) => {
-      if (res.status === 404) {
-        console.log('Backend is ready. Received 404 response.');
+      if (res.status === 200) {
+        console.log('Backend is ready!');
         process.exit(0); // Exit script successfully
       } else {
         console.log(`Unexpected response from backend: ${res.status}. Retrying in 5 seconds...`);
