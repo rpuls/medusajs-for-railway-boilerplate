6ce380f7d26d64077b4df77dbe06b965fb93d8aa
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-09-05 19:17:52 -0700

    setup paypal

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -72,6 +72,15 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: `medusa-payment-paypal`,
+    options: {
+      sandbox: process.env.PAYPAL_SANDBOX,
+      clientId: process.env.PAYPAL_CLIENT_ID,
+      clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+      authWebhookId: process.env.PAYPAL_AUTH_WEBHOOK_ID,
+    },
+  },
 ];
 
 const modules = {
