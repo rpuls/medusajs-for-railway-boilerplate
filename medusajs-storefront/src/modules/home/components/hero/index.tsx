9aa907ac96f83898e42e523f0c150020912ca6c2
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-09-08 18:05:46 -0700

    try backticks

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-no-repeat bg-cover bg-local bg-[center_bottom]" style={{backgroundImage: "url('https://console-production-05bc.up.railway.app/api/v1/buckets/tw-pub-1/objects/download?preview=true&prefix=20240810_175152_1.jpg&version_id=null')"}}>
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-no-repeat bg-cover bg-local bg-[center_bottom]" style={{backgroundImage: "url(`https://console-production-05bc.up.railway.app/api/v1/buckets/tw-pub-1/objects/download?preview=true&prefix=20240810_175152_1.jpg&version_id=null`)"}}>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
