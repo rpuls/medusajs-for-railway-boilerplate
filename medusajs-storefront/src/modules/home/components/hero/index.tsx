2c8d9647fe63b547ad072402b025ccf82a78644d
trogdor <trogdor@trogdors-MacBook-Air.local>
2024-09-08 18:02:08 -0700

    wip hero

--- medusajs-storefront/src/modules/home/components/hero/index.tsx
+++ medusajs-storefront/src/modules/home/components/hero/index.tsx
@@ -3,20 +3,19 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-no-repeat bg-cover bg-local bg-[center_bottom]" style={{backgroundImage: "url('https://console-production-05bc.up.railway.app/api/v1/buckets/tw-pub-1/objects/download?preview=true&prefix=20240810_175152_1.jpg&version_id=null')"}}>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-8xl font-normal tracking-wider text-sky-200"
           >
-            THE TONKBUILT BUILDS STORE IS COMING.
+            TONKA'S WORLD
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Are you ready?
           </Heading>
         </span>
       </div>
